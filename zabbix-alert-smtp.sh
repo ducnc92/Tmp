8faa595a589461526b734d77150312c2f3143abe
Nguyen Cong Duc <nguyencongduc3112@gmail.com>
2014-11-22 16:52:41 +0700

    Email Script for zabbix

---
+++ zabbix-alert-smtp.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Zabbix SMTP Alert script for gmail.
+"""
+
+import sys
+import smtplib
+from email.MIMEText import MIMEText
+from email.Header import Header
+from email.Utils import formatdate
+
+# Mail Account
+MAIL_ACCOUNT = 'zabbixalert'
+#MAIL_PASSWORD = ''
+MAIL_PASSWORD = 'yourpasswordhere'
+
+# Sender Name
+SENDER_NAME = 'monitoring'
+
+# Mail Server
+SMTP_SERVER = 'smtp.your.domain.com'
+SMTP_PORT = 25
+# TLS
+Check = True
+
+def send_mail(recipient, subject, body, encoding='utf-8'):
+    session = None
+    msg = MIMEText(body, 'plain', encoding)
+    msg['Subject'] = Header(subject, encoding)
+    msg['From'] = Header(SENDER_NAME, encoding)
+    msg['To'] = recipient
+    msg['Date'] = formatdate()
+    try:
+        session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+        if Check:
+            session.ehlo()
+#            session.starttls()
+#            session.ehlo()
+            session.login(MAIL_ACCOUNT, MAIL_PASSWORD)
+        session.sendmail(MAIL_ACCOUNT, recipient, msg.as_string())
+    except Exception as e:
+        raise e
+    finally:
+        # close session
+        if session:
+            session.quit()
+
+if __name__ == '__main__':
+    """
+    recipient = sys.argv[1]
+    subject = sys.argv[2]
+    body = sys.argv[3]
+    """
+    if len(sys.argv) == 4:
+        send_mail(
+            recipient=sys.argv[1],
+            subject=sys.argv[2],
+            body=sys.argv[3])
+    else:
+        print u"""requires 3 parameters (recipient, subject, body)
+\t$ zabbix-gmail.sh recipient subject body
+"""
